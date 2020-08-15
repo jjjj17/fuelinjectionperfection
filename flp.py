95e4da17a429903ea1071f17063d81e7914489e6
javierjavier <jrojasr94@live.cl>
2020-08-15 00:56:23 -0400

    first working version, not optimized

--- flp.py
+++ flp.py
@@ -4,14 +4,13 @@ def solution(n):
     result = int(n)
     counter = 0
     while result != 1:
-        power = int(math.log (result,2) + 0.5)
         if result%2 == 0:
             result = result/2
             counter += 1
         elif result%2 != 0:
             r1 = result-1
             r2 = result+1
-            if 2 ** power == r2:
+            if (math.log(r2, 2).is_integer() and math.log(r1, 2).is_integer() == False):
                 result = r2
                 counter += 1
             else:
@@ -23,4 +22,4 @@ def solution(n):
 
 #el 3 está malo
 
-print(solution('4'))
+print(solution("1213254654561325851545454545454545122165465465445645641513213213213213246545415646547871213254654561325851545454545454545122165465465445645641513213213213213246545415646547871213254654561325851545454545454545122165465465445645641513213213213213246545415646547871213254654561325851545454545454545122165"))
