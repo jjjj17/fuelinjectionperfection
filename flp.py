bc7bccf946ffa84b3e5e0dc9255f92251649c72d
javierjavier <jrojasr94@live.cl>
2020-08-17 21:32:20 -0400

    swap solution to bit manipulation of powers of 2

--- flp.py
+++ flp.py
@@ -1,25 +1,18 @@
-import math
-
-def solution(n):
-    result = int(n)
+def solution(s):
     counter = 0
-    while result != 1:
-        if result%2 == 0:
-            result = result/2
-            counter += 1
-        elif result%2 != 0:
-            r1 = result-1
-            r2 = result+1
-            if (math.log(r2, 2).is_integer() and math.log(r1, 2).is_integer() == False):
-                result = r2
+    num = int(s)
+    while num != 1:
+        if num&1 == 1:
+            if ((num+1&num) > (num-1&num-2)) or num == 3:
+                num -= 1
                 counter += 1
             else:
-                result = r1
+                num += 1
                 counter += 1
+        elif num&1 == 0:
+            num = num >> 1
+            counter += 1
     else:
         return(counter)
 
-
-#el 3 está malo
-
-print(solution("1213254654561325851545454545454545122165465465445645641513213213213213246545415646547871213254654561325851545454545454545122165465465445645641513213213213213246545415646547871213254654561325851545454545454545122165465465445645641513213213213213246545415646547871213254654561325851545454545454545122165"))
+print(solution("3"))
